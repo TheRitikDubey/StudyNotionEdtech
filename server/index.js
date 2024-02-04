3dbce24910a8af35ec9e6dce3467142dd24a0b9e
Ritik Dubey <ritikdubey@Ritik-Dubey.local>
2024-02-04 13:25:41 +0530

    Added the update model of the studyNotion app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
 // jai Shri Ganesh 
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`THE SERVER IS UP AND RUNNING AT PORT ${PORT}`);
+  });
